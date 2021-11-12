d964367df5083e86587513f5484cf953cccbdab4
MP0 <ngao4@illinois.edu>
2021-11-11 21:28:35 -0600

    add draw class

---
+++ draw.cpp
@@ -0,0 +1,6 @@
+/**
+ * This is the draw.cpp which will be used to draw the lines on the global map.
+*/
+
+#include "draw.h"
+
---
+++ draw.h
@@ -0,0 +1,15 @@
+/**
+ * This is the draw.h which will be used to draw the lines on the global map.
+*/
+#pragma once
+
+
+class draw {
+    public:
+/**
+ * default constructor
+ */
+    draw() { };
+    private:
+
+};
