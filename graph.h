4e0de11d6aa86666681e30db197aec12df5a29eb
Gabriel Gao <ngao4@siebl-0224-36.ews.illinois.edu>
2021-11-30 21:38:16 -0600

    write relevant functions and constructors of edge

--- graph.h
+++ graph.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -17,8 +18,77 @@ class Graph {
         };
 
         struct Edge {
-            pair<string, string> edge_id;
+            // pair<string, string> edge_id;
+            string source_id;
+            string destination_id;
             double distance;
+
+            /**
+            * Constructor with unweighted edge
+            * @param sourceAirport - sourceAirport
+            * @param destinationAirport - destinationAirport
+            */
+            Edge(string sourceAirport, string destinationAirport)
+                : source_id(sourceAirport), destination_id(destinationAirport), distance(-1)
+            {/* empty*/}
+
+            /**
+            * Constructor with weighted edge
+            * @param sourceAirport - sourceAirport
+            * @param destinationAirport - destinationAirport
+            * @param d - the distance between the two airports(the weight of the edge)
+            */
+             Edge(string sourceAirport, string destinationAirport, double d)
+                : source_id(sourceAirport), destination_id(destinationAirport), distance(d)
+            {/* empty*/}
+
+            /**
+            * Default Constructor 
+            */
+            Edge()
+                : source_id(""), destination_id(""), distance(-1)
+            {/* empty*/}
+
+            /**
+            * operator< of Edge
+            * @param other - the other edge to compare
+            */
+            bool operator<(const Edge & other) {
+                return distance < other.distance;
+            }
+
+            /**
+            * operator== of Edge
+            * @param other - the other edge to compare
+            */
+            bool operator==(const Edge & other) {
+                if (this->source_id == other.source_id && this->destination_id == other.destination_id) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+            /**
+            * Get distance of edge(weight)
+            */
+            double getDistance() {
+                return distance;
+            }
+
+            /**
+            * Get sourseAirportId of edge
+            */
+            string getSource() {
+                return source_id;
+            }
+
+            /**
+            * Get DestinationAirportId of edge
+            */
+            string getDestination() {
+                return destination_id;
+            }
         }; 
     
 };
