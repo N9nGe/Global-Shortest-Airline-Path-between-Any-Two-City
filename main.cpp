283fce8c3340b4212d70425cedcd6669032ad234
Jeffrey Zhai <zhai11@siebl-0224-22.ews.illinois.edu>
2021-12-08 01:34:08 -0600

    finished test drawmap

--- main.cpp
+++ main.cpp
@@ -73,9 +73,12 @@ int main()
     //test drawmap
     Graph::Vertex YYC("178", 51.113899231, -114.019996643);
     Graph::Vertex FCO("1555", 41.8002778, 12.2388889);
-    map<string, string> theMap = func.Dijkstra(g, YYC, FCO);
-    cout << theMap.size() << endl;
-    draw.drawmap("data/routes.dat", "data/airports.dat", YYC, FCO);
+    Graph::Vertex PEK("3364",40.080101013183594,116.58499908447266);
+    Graph::Vertex CGO("3375",34.519699096699995,113.841003418);
+    Graph::Vertex ORD("3830",41.9786,-87.9048);
+    // map<string, string> theMap = func.Dijkstra(g, CGO, ORD);
+    // cout << theMap.size() << endl;
+    draw.drawmap("data/routes.dat", "data/airports.dat", CGO, ORD);
 
     Draw draw_2;
     PNG * graph_2 = draw_2.open();
@@ -92,7 +95,6 @@ int main()
     }
     
     graph_2->writeToFile("all_airlines.png");
-
 
     return 0;
 }
